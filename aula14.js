66b48e01ea7ed9ed9d7d54525363d937ab674f34
uuillians <109052291+uuillians@users.noreply.github.com>
2022-07-11 15:25:34 -0300

    Fix 2

--- aula14.js
+++ aula14.js
@@ -29,7 +29,7 @@ btnEnviar.addEventListener('click', function (e) {
     let idade  = cxIdade.value
     let peso   = cxPeso.value
     let altura = cxAltura.value
-    let imc    = (peso / (altura * idade)).toFixed(1)
+    let imc    = (peso / (altura * idade)).toFixed(2)
 
     console.log(nome)
     console.log(idade)
